03901c41de8c22dbcd2448513dc72acfd70a3418
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-21 11:14:26 -0700

    Create podfile and add necessary pods

---
+++ SocialNetwork/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SocialNetwork
+//
+//  Created by spencerdezartsmith on 3/16/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
