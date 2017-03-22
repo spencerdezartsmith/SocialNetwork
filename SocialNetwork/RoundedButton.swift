39a6d58837e99d1492a3a4a02acd7a177c889bcb
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-22 13:15:36 -0700

    Add custom rounded button class

--- SocialNetwork/RoundedButton.swift
+++ SocialNetwork/RoundedButton.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class RoundedButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+        layer.cornerRadius = 5.0
     }
-    */
 
 }
