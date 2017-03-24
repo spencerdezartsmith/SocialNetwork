a444c2af53f88d3827cc7e67a847b2b19f170738
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-24 16:48:15 -0700

    Set up SwiftKeychainWrapper pod. Add uid constant. Add complete signin func to save uid to keychain

--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'SocialNetwork' do
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
   pod 'FBSDKLoginKit'
+  pod 'SwiftKeychainWrapper'
 
 end
--- SocialNetwork/SignInVC.swift
+++ SocialNetwork/SignInVC.swift
@@ -10,6 +10,7 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
+import SwiftKeychainWrapper
 
 class SignInVC: UIViewController {
 
@@ -19,11 +20,6 @@ class SignInVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func facebookBtnTapped(_ sender: Any) {
@@ -50,12 +46,21 @@ class SignInVC: UIViewController {
             FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
                 if error == nil {
                     print("User Authenticated with Email and Password")
+                    
+                    if let user = user {
+                        self.completeSignIn(id: user.uid)
+                    }
+                    
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
                         if error != nil {
                             print("Unable to create new user in Firebase: \(error)")
                         } else {
                             print("New user successfully created and authenticated")
+                            
+                            if let user = user {
+                                self.completeSignIn(id: user.uid)
+                            }
                         }
                     })
                 }
@@ -72,8 +77,16 @@ class SignInVC: UIViewController {
                 print("Unable to Authenticate with Firebase")
             } else {
                 print("Successfully Authenticated with Firebase")
+                
+                if let user = user {
+                   self.completeSignIn(id: user.uid)
+                }
             }
         })
+    }
+    
+    func completeSignIn(id: String) {
+        KeychainWrapper.standard.set(id, forKey: KEY_UID)
     }
 
 }
