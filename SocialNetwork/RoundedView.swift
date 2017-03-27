0dd14a4d0660ce802040c13e8ec947a811cd6078
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-27 12:43:04 -0700

    Add new rounded image custom class

---
+++ SocialNetwork/RoundedView.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedView.swift
+//  SocialNetwork
+//
+//  Created by spencerdezartsmith on 3/27/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import UIKit
+
+class RoundedView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        contentMode = .scaleAspectFit
+        layer.cornerRadius = 5.0
+    }
+    
+}
