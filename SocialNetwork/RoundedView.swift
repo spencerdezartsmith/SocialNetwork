ac44124fb3ab171bfa3b3b87dd000ccab8d8ddb6
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-27 13:32:30 -0700

    Complete the feed prototype cell

--- SocialNetwork/RoundedView.swift
+++ SocialNetwork/RoundedView.swift
@@ -17,7 +17,6 @@ class RoundedView: UIImageView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        contentMode = .scaleAspectFit
         layer.cornerRadius = 5.0
     }
     
