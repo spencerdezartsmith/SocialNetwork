1f6d421610dbdb8491479342e624231d9b517a1c
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-03-27 18:04:44 -0700

    Create a custom circle view class

---
+++ SocialNetwork/CircleView.swift
@@ -0,0 +1,17 @@
+//
+//  CircleView.swift
+//  SocialNetwork
+//
+//  Created by spencerdezartsmith on 3/27/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+}
